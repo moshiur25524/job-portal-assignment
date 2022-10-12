90120d215a3084d7947dd6ec492f1639503de01e
moshiur25524 <moshiur.rahman25524@gmail.com>
2022-10-12 18:59:10 +0600

    created the jobsSchema and model

--- models/jobs.js
+++ models/jobs.js
@@ -1,1 +1,35 @@
 const mongoose = require('mongoose')
+
+const jobSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, 'You have to provide the name'],
+        trim: true,
+        unique: [true, 'Name must be unique'],
+        lowercase: true,
+        minLength: [3, 'Name must be at least 3 character'],
+        maxLength: [100, 'Too large name']
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    job_Type:{
+        type: String,
+        required: true
+    },
+    salary:{
+        type: Number,
+        required: true
+    }
+},{
+    timestamps: true
+})
+
+const Jobs = mongoose.model('Jobs', jobSchema)
+
+module.exports = Jobs
